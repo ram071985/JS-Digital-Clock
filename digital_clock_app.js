8067d7c7ebf0f55d5126ff2450d6c17ccb655c5d
ram071985 <58052336+ram071985@users.noreply.github.com>
2019-11-28 21:40:06 -0600

    Updated javascript
     by adding if and else for 12/24 hour time

--- digital_clock_app.js
+++ digital_clock_app.js
@@ -1,5 +1,7 @@
 /* Digital Clock App by Reid Muchow */
 
+/* Digital Clock App by Reid Muchow */
+
 let todayDate = new Date();
 let day = todayDate.getDate();
 let month = todayDate.getMonth();
@@ -14,8 +16,44 @@ let hours = timeDate.getHours();
 let minutes = timeDate.getMinutes();
 let seconds = timeDate.getSeconds();
 
-let elTime = document.getElementById('time');
-elTime.textContent = hours + ':' + minutes + ':' + seconds;
+let clock = document.getElementById('time');
+clock.textContent = hours + ':' + minutes + ':' + seconds;
+if (hours == 13) {
+  let clock = document.getElementById('time');
+  clock.textContent = 1 + hours + ':' + minutes + ':' + seconds; 
+if (hours == 14) {
+  let clock = document.getElementById('time');
+    clock.textContent = 2 + hours + ':' + minutes + ':' + seconds; 
+if (hours == 15) {
+  let clock = document.getElementById('time');
+      nonMilitary.textContent = 3 + hours + ':' + minutes + ':' + seconds; 
+if (hours == 16) {
+  let nonMilitary = document.getElementById('time');
+        nonMilitary.textContent = 4 + hours + ':' + minutes + ':' + seconds; 
+if (hours == 17) {
+  let nonMilitary = document.getElementById('time');
+          nonMilitary.textContent = 5 + hours + ':' + minutes + ':' + seconds; 
+if (hours == 18) {
+  let nonMilitary = document.getElementById('time');
+  nonMilitary.textContent = 6 + hours + ':' + minutes + ':' + seconds;
+if (hours == 19) {
+  let nonMilitary = document.getElementById('time');
+    nonMilitary.textContent = 7 + hours + ':' + minutes + ':' + seconds;
+if (hours == 20) {
+  let nonMilitary = document.getElementById('time');
+      nonMilitary.textContent = 8 + hours + ':' + minutes + ':' + seconds;
+if (hours == 21) {
+  let nonMilitary = document.getElementById('time');
+    nonMilitary.textContent = 9 + hours + ':' + minutes + ':' + seconds;
+if (hours == 22) {
+  let nonMilitary = document.getElementById('time');
+      nonMilitary.textContent = 10 + hours + ':' + minutes + ':' + seconds;
+if (hours == 18) {
+  let nonMilitary = document.getElementById('time');
+    nonMilitary.textContent = 11 + hours + ':' + minutes + ':' + seconds;
+if (hours == 18) {
+  let nonMilitary = document.getElementById('time');
+    nonMilitary.textContent = 12 + hours + ':' + minutes + ':' + seconds;
 }
 
 setInterval(time, 1000);
